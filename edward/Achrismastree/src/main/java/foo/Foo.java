a375e2a3654643fa4fff767374f83d71482bd2e4
DG745 <edwardxichen@gmail.com>
2019-03-28 13:49:23 -0700

    Christmas Tree 圣诞树

---
+++ edward/Achrismastree/src/main/java/foo/Foo.java
@@ -0,0 +1,57 @@
+package foo;
+
+public class Foo {
+    public static void main(String[] args){
+
+        System.out.println ("          1          ");
+        System.out.println ("         2 2         ");
+        System.out.println ("        3   3        ");
+        System.out.println ("       4     4       ");
+        System.out.println ("      5       5      ");
+        System.out.println ("       6     6       ");
+        System.out.println ("      7       7      ");
+        System.out.println ("     8         8     ");
+        System.out.println ("    9           9    ");
+        System.out.println ("   0             0   ");
+        System.out.println ("    VAVAVAVAVAVAV    ");
+
+        System.out.println ("This is a bad Christmas tree. " + "Here's a better one:");
+
+        System.out.println ("          /\\          ");
+        System.out.println ("         /  \\         ");
+        System.out.println ("        /    \\        ");
+        System.out.println ("       /      \\       ");
+        System.out.println ("        /    \\        ");
+        System.out.println ("       /      \\       ");
+        System.out.println ("      /        \\      ");
+        System.out.println ("     /          \\     ");
+        System.out.println ("      /        \\      ");
+        System.out.println ("     /          \\     ");
+        System.out.println ("    /            \\    ");
+        System.out.println ("   /              \\   ");
+        System.out.println ("  /________________\\  ");
+        System.out.println ("         ||||          ");
+        System.out.println ("         ||||          ");
+
+        System.out.println ("-_-");
+        System.out.println("Potatoes are good foods for Communists");
+        System.out.println("Let's Decorate it!");
+
+        System.out.println ("          /\\          ");
+        System.out.println ("         /  \\         ");
+        System.out.println ("        / 1  \\        ");
+        System.out.println ("       / 9  @ \\       ");
+        System.out.println ("        /  ( \\        ");
+        System.out.println ("       /  )   \\       ");
+        System.out.println ("      /  7   = \\      ");
+        System.out.println ("     / #   -  + \\     ");
+        System.out.println ("      /   }    \\      ");
+        System.out.println ("     /  !   ^   \\     ");
+        System.out.println ("    / <   &   $  \\    ");
+        System.out.println ("   / 8   *   0 >  \\   ");
+        System.out.println ("  /________________\\  ");
+        System.out.println ("         ||||          ");
+        System.out.println ("         ||||          ");
+
+    }
+}

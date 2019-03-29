7d96806ea1396888ed9835f53f4efac85aa87883
DG745 <edwardxichen@gmail.com>
2019-03-29 11:41:15 -0700

    New and Improved Java thing! Now with 100% more loops, 10% more print lines, 200% more variables, a few returns, a scanner, and like 100% more if/else statements!

--- edward/Achrismastree/src/main/java/foo/Foo.java
+++ edward/Achrismastree/src/main/java/foo/Foo.java
@@ -1,59 +1,78 @@
 package foo;
+import java.util.Scanner;
 
 public class Foo {
     public static void main(String[] args){
+        System.out.println("Enter a positive integer number");
+        Scanner number_receiver = new Scanner(System.in);
 
-        System.out.println ("          1          ");
-        System.out.println ("         2 2         ");
-        System.out.println ("        3   3        ");
-        System.out.println ("       4     4       ");
-        System.out.println ("      5       5      ");
-        System.out.println ("       6     6       ");
-        System.out.println ("      7       7      ");
-        System.out.println ("     8         8     ");
-        System.out.println ("    9           9    ");
-        System.out.println ("   0             0   ");
-        System.out.println ("    VAVAVAVAVAVAV    ");
+        int y = 0;
+        int x = number_receiver.nextInt();
 
-        System.out.println ("This is a bad Christmas tree. " + "Here's a better one:");
+        if(x < 1){
+            System.out.println("!@#*@*&$*@&$*@(!))@$&*(@$&*!&(@%@)$&**(@!");
+            return;
+        }
+        while(0 == 0) {
+            System.out.println("          1          ");
+            System.out.println("         2 2         ");
+            System.out.println("        3   3        ");
+            System.out.println("       4     4       ");
+            System.out.println("      5       5      ");
+            System.out.println("       6     6       ");
+            System.out.println("      7       7      ");
+            System.out.println("     8         8     ");
+            System.out.println("    9           9    ");
+            System.out.println("   0             0   ");
+            System.out.println("    VAVAVAVAVAVAV    ");
 
-        System.out.println ("          /\\          ");
-        System.out.println ("         /  \\         ");
-        System.out.println ("        /    \\        ");
-        System.out.println ("       /      \\       ");
-        System.out.println ("        /    \\        ");
-        System.out.println ("       /      \\       ");
-        System.out.println ("      /        \\      ");
-        System.out.println ("     /          \\     ");
-        System.out.println ("      /        \\      ");
-        System.out.println ("     /          \\     ");
-        System.out.println ("    /            \\    ");
-        System.out.println ("   /              \\   ");
-        System.out.println ("  /________________\\  ");
-        System.out.println ("         ||||          ");
-        System.out.println ("         ||||          ");
+            System.out.println("This is a bad Christmas tree. " + "Here's a better one:");
 
-        System.out.println ("-_-");
-        System.out.println("Potatoes are good foods for Communists");
-        System.out.println("Let's Decorate it!");
+            System.out.println("          /\\          ");
+            System.out.println("         /  \\         ");
+            System.out.println("        /    \\        ");
+            System.out.println("       /      \\       ");
+            System.out.println("        /    \\        ");
+            System.out.println("       /      \\       ");
+            System.out.println("      /        \\      ");
+            System.out.println("     /          \\     ");
+            System.out.println("      /        \\      ");
+            System.out.println("     /          \\     ");
+            System.out.println("    /            \\    ");
+            System.out.println("   /              \\   ");
+            System.out.println("  /________________\\  ");
+            System.out.println("         ||||          ");
+            System.out.println("         ||||          ");
 
-        System.out.println ("          /\\          ");
-        System.out.println ("         /  \\         ");
-        System.out.println ("        / 1  \\        ");
-        System.out.println ("       / 9  @ \\       ");
-        System.out.println ("        /  ( \\        ");
-        System.out.println ("       /  )   \\       ");
-        System.out.println ("      /  7   = \\      ");
-        System.out.println ("     / #   -  + \\     ");
-        System.out.println ("      /   }    \\      ");
-        System.out.println ("     /  !   ^   \\     ");
-        System.out.println ("    / <   &   $  \\    ");
-        System.out.println ("   / 8   *   0 >  \\   ");
-        System.out.println ("  /________________\\  ");
-        System.out.println ("         ||||          ");
-        System.out.println ("         ||||          ");
+            System.out.println("-_-");
+            System.out.println("Potatoes are good foods for Communists");
+            System.out.println("Let's Decorate it!");
 
-        System.out.println("Potato");
+            System.out.println("          /\\          ");
+            System.out.println("         /  \\         ");
+            System.out.println("        / 1  \\        ");
+            System.out.println("       / 9  @ \\       ");
+            System.out.println("        /  ( \\        ");
+            System.out.println("       /  )   \\       ");
+            System.out.println("      /  7   = \\      ");
+            System.out.println("     / #   -  + \\     ");
+            System.out.println("      /   }    \\      ");
+            System.out.println("     /  !   ^   \\     ");
+            System.out.println("    / <   &   $  \\    ");
+            System.out.println("   / 8   *   0 >  \\   ");
+            System.out.println("  /________________\\  ");
+            System.out.println("         ||||          ");
+            System.out.println("         ||||          ");
 
+            System.out.println("Potato");
+
+            y++;
+            if(x == y){
+                System.out.println("STOP!!!");
+                return;
+            } else{
+                System.out.println("LOOP!!!!!");
+            }
+        }
     }
 }

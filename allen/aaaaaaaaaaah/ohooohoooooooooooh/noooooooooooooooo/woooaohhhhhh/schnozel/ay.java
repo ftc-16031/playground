9a8e8e9cbcc2fce94e5c887458b190956a5d3ba6
gronkybong <48972370+gronkybong@users.noreply.github.com>
2019-03-30 11:40:48 -0700

    required christmas tre

--- allen/aaaaaaaaaaah/ohooohoooooooooooh/noooooooooooooooo/woooaohhhhhh/schnozel/ay.java
+++ allen/aaaaaaaaaaah/ohooohoooooooooooh/noooooooooooooooo/woooaohhhhhh/schnozel/ay.java
@@ -29,13 +29,17 @@ public class ay {
 		
 		if (password1.toLowerCase().equals(response2)) {
 			System.out.println("\na christmas frickin tree");
-			System.out.println("     /\\");
-			System.out.println("    /  \\");
-			System.out.println("   /_  _\\");
-			System.out.println("    /  \\");
-			System.out.println("   /    \\");
-			System.out.println("  /______\\");
-			System.out.println("     ||");
+			System.out.println("        1");
+			System.out.println("       2 2");
+			System.out.println("      3   3");
+			System.out.println("     4     4");
+			System.out.println("    5       5");
+			System.out.println("     6     6");
+			System.out.println("    7       7");
+			System.out.println("   8         8");
+			System.out.println("  9           9");
+			System.out.println(" 0             0");
+			System.out.println("  VAVAVAVAVAVAV");
 			
 		} else {
 			System.out.println("ur trash lmao");
